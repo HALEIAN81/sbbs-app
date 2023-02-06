dc1acdd97aff2659611005fd37e0113dd8569eef
Roland Green <roland.green@highpowerdata.com>
2023-02-06 14:45:05 -0800

    bug fixes, page edits

--- src/pages/author.jsx
+++ src/pages/author.jsx
@@ -9,14 +9,14 @@ const Author = () => {
         and creative endeavors is to uncover that mystery every day" - Hale Verde. 
 
         The story of Jackson, Jira and Melissa is a budding story of individual heroism, dedication and the 
-        depthness of character even in the world of fiction. I hope that those that read our book (and those that follow)
+        depthness of character even in the world of fiction. I hope that those that read our book and those that follow
         can share in that journey and make it their own. BlueHand is a mysterious force and as we work through the series
         I hope that our readers will discover a love for that 'unwinding' of their mystery and learn more about Scotland, 
         Washington and as such - Scotland Backyard.
 
         Please look out for Scotland Backyard - Volume II - The Warlock's Cabal coming out Q3 2023 on Amazon. 
 
-        Thank you all for supporting this journey.
+        Thank you all for supporting this journey."
       </p>
     </div>
   );
