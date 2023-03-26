9a5e4b54dec13cc46f460abaf935989b6416c43a
Roland Green <roland.green@highpowerdata.com>
2023-03-26 01:31:17 -0700

    added image href, added detail section to main page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,7 +8,7 @@ const index = () => {
           <h1 className="heading glow sb-title">
           SCOTLAND BACKYARD BOOK SERIES
           </h1>
-          <div className="headerImg"></div>
+          <a href="https://www.amazon.com/Scotland-Backyard-Knights-Hale-Verde-ebook/dp/B09NKVJTKK/ref=sr_1_1?crid=DKHA4LZM4WM0&keywords=Scotland+Backyard&qid=1679819319&sprefix=scotland+backyard%2Caps%2C143&sr=8-1" target="_blank"><div className="headerImg"></div></a>
         </section>
         <section className="news">
           <h2 className="newsHeading glow sb-title">The Story of The Best Young Sleuth in the World</h2>
@@ -20,15 +20,100 @@ const index = () => {
               </p>
               <p className="newsTitleContent">Welcome to Scotland Backyard.</p>
             </div>
-            {/* <div className="newsRight">
+            <section className="news portfolio">
+          <h2 className="portfolioHeading glow">The Plot Thickens</h2>
+          <div className="newsContainer">
+            <div className="newsTitle">
+              <h4 className="portfolioTitle1">Scotland, Washington</h4>
+              <p className="newsTitleContent">
+              Eiusmod do sint sunt proident do consequat adipisicing amet non pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum et veniam consectetur excepteur elit minim deserunt anim cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex. Lorem laboris sint ea Lorem tempor culpa officia consequat laboris.
+              </p>
+            </div>
+            <div className="newsRight">
               <img
-                className="newsRightImg"
+                className="portfolioImg1"
                 src={
-                  "https://images.unsplash.com/photo-1497008386681-a7941f08011e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+                  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
                 }
                 alt="newsImage"
               />
-            </div> */}
+            </div>
+          </div>
+
+          <div className="newsContainer ">
+            <div className="newsRight">
+              <img
+                className="portfolioImg2"
+                src={
+                  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                }
+                alt="newsImage"
+              />
+            </div>
+            <div className="newsTitle">
+              <h4 className="portfolioTitle2">Scotland United Middle School</h4>
+              <p className="newsTitleContent">
+              Eiusmod do sint sunt proident do consequat adipisicing amet non pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum et veniam consectetur excepteur elit minim deserunt anim cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex. Lorem laboris sint ea Lorem tempor culpa officia consequat laboris.
+              </p>
+            </div>
+          </div>
+
+          <div className="newsContainer">
+            <div className="newsTitle">
+              <h4 className="portfolioTitle3">BlueHand Corporation</h4>
+              <p className="newsTitleContent">
+              Eiusmod do sint sunt proident do consequat adipisicing amet non pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum et veniam consectetur excepteur elit minim deserunt anim cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex. Lorem laboris sint ea Lorem tempor culpa officia consequat laboris.
+              </p>
+            </div>
+            <div className="newsRight">
+              <img
+                className="portfolioImg3"
+                src={
+                  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                }
+                alt="newsImage"
+              />
+            </div>
+          </div>
+          
+          <div className="newsContainer">
+            <div className="newsRight">
+              <img
+                className="portfolioImg4"
+                src={
+                  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                }
+                alt="newsImage"
+              />
+            </div>
+            <div className="newsTitle">
+              <h4 className="portfolioTitle4">The Shaw Legacy</h4>
+              <p className="newsTitleContent">
+              Eiusmod do sint sunt proident do consequat adipisicing amet non pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum et veniam consectetur excepteur elit minim deserunt anim cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex. Lorem laboris sint ea Lorem tempor culpa officia consequat laboris.
+              </p>
+            </div>
+          </div>
+                
+          <div className="newsContainer">
+            <div className="newsTitle">
+              <h4 className="portfolioTitle5">The Weirdness</h4>
+              <p className="newsTitleContent">
+                Eiusmod do sint sunt proident do consequat adipisicing amet non pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum et veniam consectetur excepteur elit minim deserunt anim cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex. Lorem laboris sint ea Lorem tempor culpa officia consequat laboris.
+              </p>
+            </div>
+            <div className="newsRight">
+              <img
+                className="portfolioImg5"
+                src={
+                  "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+                }
+                alt="newsImage"
+              />
+            </div>
+            
+          </div>
+          
+        </section>
           </div>
         </section>
         <section className="archives">
