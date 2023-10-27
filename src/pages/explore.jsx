087a8bf6529e8f2965f4025cb5a54795d9c5b817
Roland Green <roland.green@highpowerdata.com>
2023-10-27 14:01:37 -0700

    updated explore page

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -1,171 +1,147 @@
 import React from "react";
 
+
 const Explore = () => {
-  return (
-    <>
-      <div className="explore">
-        <h3 style={{margin:"0px"}} className="pageTitle glow sb-title heading2">
-          Explore Scotland Backyard
-        </h3>
-        <a
-        style={{width:"100%"}}
-          href="https://www.amazon.com/Scotland-Backyard-Knights-Hale-Verde-ebook/dp/B09NKVJTKK/ref=sr_1_1?crid=DKHA4LZM4WM0&keywords=Scotland+Backyard&qid=1679819319&sprefix=scotland+backyard%2Caps%2C143&sr=8-1"
-          target="_blank"
-        >
-          {/* <div className="explore-img1"></div> */}
-      <img src="https://jodeaux.agency/assets/img/portfolio/portfolio-23.png" className="image1" alt="" />
+return (
+<>
+<div className="explore">
+<h3 style={{margin:"0px"}} className="pageTitle glow sb-title heading2">
+Explore Scotland Backyard
+</h3>
+<a
+style={{width:"100%"}}
+href="https://www.amazon.com/Scotland-Backyard-Knights-Hale-Verde-ebook/dp/B09NKVJTKK/ref=sr_1_1?crid=DKHA4LZM4WM0&keywords=Scotland+Backyard&qid=1679819319&sprefix=scotland+backyard%2Caps%2C143&sr=8-1"
+target="_blank"
+>
+{/* <div className="explore-img1"></div> */}
+<img src="https://jodeaux.agency/assets/img/portfolio/portfolio-23.png" className="image1" alt="" />
 
-        </a>
-        <p className="explore-text p1">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-          molestiae, a error aliquid debitis laborum. Quis fugiat dolorem
-          repellendus doloribus iure nobis, ab voluptas qui odio saepe nulla
-          laboriosam, numquam nisi? Quam inventore mollitia sequi odio? Nemo
-          voluptatem, exercitationem vel in dolorum cumque accusamus nihil aut
-          omnis est dignissimos reiciendis neque quam praesentium harum
-          obcaecati itaque assumenda deserunt doloremque! Eveniet maiores animi
-          ducimus! Eos pariatur cum, magni quisquam atque non vitae sit
-          temporibus saepe. Officiis, inventore nam illum consectetur quam
-          reiciendis consequatur temporibus eum excepturi ipsam quo, quod
-          eveniet natus magnam consequuntur dolorum fugiat suscipit
-          exercitationem ex facere? Blanditiis sequi nam excepturi ad ex rerum,
-          asperiores provident maiores dolorum at fugit, illo eligendi eum quasi
-          non, consequatur velit labore earum deleniti nulla minus dignissimos?
-          Similique corporis maxime a rerum voluptate. Ratione possimus aliquid
-          unde dolore facere, quasi laudantium veritatis, consequatur
-          necessitatibus sint, delectus dolorem ad atque animi quo alias
-          distinctio?
-        </p>
-      </div>
-      <section className="news portfolio">
-        <h2 className="portfolioHeading glow">The Plot Thickens</h2>
-        <div className="newsContainer">
-          <div className="newsTitle">
-            <h4 className="portfolioTitle1">Scotland, Washington</h4>
-            <p className="newsTitleContent p1">
-              Eiusmod do sint sunt proident do consequat adipisicing amet non
-              pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip
-              nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate
-              quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum
-              et veniam consectetur excepteur elit minim deserunt anim
-              cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex.
-              Lorem laboris sint ea Lorem tempor culpa officia consequat
-              laboris.
-            </p>
-          </div>
-          <div className="newsRight">
-            <img
-              className="portfolioImg1"
-              src={
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              }
-              alt="newsImage"
-            />
-          </div>
-        </div>
 
-        <div className="newsContainer column-reverse">
-          <div className="newsRight">
-            <img
-              className="portfolioImg2"
-              src={
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              }
-              alt="newsImage"
-            />
-          </div>
-          <div className="newsTitle">
-            <h4 className="portfolioTitle2">Scotland United Middle School</h4>
-            <p className="newsTitleContent p1">
-              Eiusmod do sint sunt proident do consequat adipisicing amet non
-              pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip
-              nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate
-              quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum
-              et veniam consectetur excepteur elit minim deserunt anim
-              cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex.
-              Lorem laboris sint ea Lorem tempor culpa officia consequat
-              laboris.
-            </p>
-          </div>
-        </div>
+</a>
+<p className="explore-text p1">
+Scotland Backyard is a charming story, made to steep our readers in mystery and heroism. The kind
+that saves the world, falls in love, reaches for personal growth and discovery for our characters.
+This is the first installment, with more on the way - we sincerely hope you will love the world we
+have carved out and follow the story through to its end.
+</p>
+</div>
+<section className="news portfolio">
+<h2 className="portfolioHeading glow">The Plot Thickens</h2>
+<div className="newsContainer">
+<div className="newsTitle">
+<h4 className="portfolioTitle1">Scotland, Washington</h4>
+<p className="newsTitleContent p1">
+Surrounded by mountains, located betwixt Bremerton and Poulsbo cities, there is Scotland, Washington.
+A strange place, filled with the extraordinary from the Navy base and all its experiments and military
+routine. There is only one main road into and out of Scotland, WA - along with all the
+</p>
+</div>
+<div className="newsRight">
+<img
+className="portfolioImg1"
+src={
+"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+}
+alt="newsImage"
+/>
+</div>
+</div>
 
-        <div className="newsContainer">
-          <div className="newsTitle">
-            <h4 className="portfolioTitle3">BlueHand Corporation</h4>
-            <p className="newsTitleContent p1">
-              Eiusmod do sint sunt proident do consequat adipisicing amet non
-              pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip
-              nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate
-              quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum
-              et veniam consectetur excepteur elit minim deserunt anim
-              cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex.
-              Lorem laboris sint ea Lorem tempor culpa officia consequat
-              laboris.
-            </p>
-          </div>
-          <div className="newsRight">
-            <img
-              className="portfolioImg3"
-              src={
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              }
-              alt="newsImage"
-            />
-          </div>
-        </div>
 
-        <div className="newsContainer column-reverse">
-          <div className="newsRight">
-            <img
-              className="portfolioImg4"
-              src={
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              }
-              alt="newsImage"
-            />
-          </div>
-          <div className="newsTitle">
-            <h4 className="portfolioTitle4">The Shaw Legacy</h4>
-            <p className="newsTitleContent p1">
-              Eiusmod do sint sunt proident do consequat adipisicing amet non
-              pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip
-              nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate
-              quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum
-              et veniam consectetur excepteur elit minim deserunt anim
-              cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex.
-              Lorem laboris sint ea Lorem tempor culpa officia consequat
-              laboris.
-            </p>
-          </div>
-        </div>
+<div className="newsContainer column-reverse">
+<div className="newsRight">
+<img
+className="portfolioImg2"
+src={
+"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+}
+alt="newsImage"
+/>
+</div>
+<div className="newsTitle">
+<h4 className="portfolioTitle2">Scotland United Middle School</h4>
+<p className="newsTitleContent p1">
+One of the first schools created when the town was founded. SUMS is built out of the remnants
+of one of the castle's that was constructed in the town under the tutelage of the towns' founder and
+elder Scotsman - Knox McGregor Sr. He was a fan of knights as he always believe he descended from
+knights and fashioned the school to hold his collection of knight statues and decor.
+</p>
+</div>
+</div>
 
-        <div className="newsContainer">
-          <div className="newsTitle">
-            <h4 className="portfolioTitle5">The Weirdness</h4>
-            <p className="newsTitleContent p1">
-              Eiusmod do sint sunt proident do consequat adipisicing amet non
-              pariatur aliquip. Labore laborum id in do fugiat cupidatat aliquip
-              nisi cupidatat ut mollit. Aliqua elit nulla ad id magna voluptate
-              quis exercitation esse eiusmod fugiat occaecat nostrud eu. Cillum
-              et veniam consectetur excepteur elit minim deserunt anim
-              cupidatat. Enim minim pariatur quis pariatur id culpa ad quis ex.
-              Lorem laboris sint ea Lorem tempor culpa officia consequat
-              laboris.
-            </p>
-          </div>
-          <div className="newsRight">
-            <img
-              className="portfolioImg5"
-              src={
-                "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              }
-              alt="newsImage"
-            />
-          </div>
-        </div>
-      </section>
-    </>
-  );
+
+<div className="newsContainer">
+<div className="newsTitle">
+<h4 className="portfolioTitle3">BlueHand Corporation</h4>
+<p className="newsTitleContent p1">
+BlueHand Corporation has a solid foothold over the town, founded in the 1920s - it has its hands
+into everything from supplying the military, to research and development in the tech and medical industry,
+advanced physics, quantum mechanics and artificial intelligence. What is its real mission? No one knows, but Jackson and Jiro
+have dedicated there young lives to uncovering its atrocities and wrong-doings.
+</p>
+</div>
+<div className="newsRight">
+<img
+className="portfolioImg3"
+src={
+"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+}
+alt="newsImage"
+/>
+</div>
+</div>
+
+
+<div className="newsContainer column-reverse">
+<div className="newsRight">
+<img
+className="portfolioImg4"
+src={
+"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+}
+alt="newsImage"
+/>
+</div>
+<div className="newsTitle">
+<h4 className="portfolioTitle4">The Shaw Legacy</h4>
+<p className="newsTitleContent p1">
+Jackson Shaw and his mother Clara Shaw live in the town of Scotland, Washington making a life,
+day by day. Jackson, ever the sleuth and obsessed hockey player is fascinated with uncovering
+all the mysteries of Scotland, Washington.
+</p>
+</div>
+</div>
+
+
+<div className="newsContainer">
+<div className="newsTitle">
+<h4 className="portfolioTitle5">The Weirdness</h4>
+<p className="newsTitleContent p1">
+Between the Navy's experiments and 'accidental mishaps' of BlueHand's secret labs,
+the Scotland University's bolstering hyper-smart young college students and that the area
+seems to attract the greatest minds from all over the world and you have yourself a formula
+for what has been affectionately called "The Weirdness". Put that together with ancient groups
+laying claim to the Native American ruins strown about the town, the secret societies of
+would-be vampires, werewolves, immortals and yes, "The Weirdness" begins to have meaning.
+</p>
+</div>
+<div className="newsRight">
+<img
+className="portfolioImg5"
+src={
+"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+}
+alt="newsImage"
+/>
+</div>
+</div>
+</section>
+</>
+);
 };
 
+
 export default Explore;
+
+
+
