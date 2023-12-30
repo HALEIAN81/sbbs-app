fb931097cb7ff84d91241ad4c4e52472917588c0
Roland Green <roland.green@highpowerdata.com>
2023-12-30 10:06:09 -0800

    updated shaw legacy img

--- src/pages/explore.jsx
+++ src/pages/explore.jsx
@@ -92,7 +92,7 @@ alt="newsImage"
 <img
 className="portfolioImg4"
 src={
-"https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
+"https://jodeaux.agency/assets/img/portfolio/portfolio-46.png"
 }
 alt="newsImage"
 />
