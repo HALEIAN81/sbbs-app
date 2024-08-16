b49a2f1e195fa60cbf2cc44c9188b22d8bd2c975
Roland Green <roland.green@highpowerdata.com>
2024-08-16 12:00:38 -0700

    blog page 2 fix

--- src/pages/blog-post-2.jsx
+++ src/pages/blog-post-2.jsx
@@ -4,24 +4,24 @@ function Blog() {
   const [blogPost, setBlogPost] = useState(null);
 
   useEffect(() => {
-    fetch("blogPost.json")
+    fetch("blogPost2.json")
       .then((response) => response.json())
       .then((data) => setBlogPost(data[1])); // Access the first post
   }, []);
 
   return (
     <div className="blog-section">
-      {blogPost && (
+      {blogPost2 && (
         <>
           <h2 className="blog-title" style={{ color: "white" }}>
-            {blogPost.title}
+            {blogPost2.title}
           </h2>
-          <img src={blogPost.image} alt={blogPost.title} />
+          <img src={blogPost2.image} alt={blogPost2.title} />
           <p className="blog-description">{blogPost.description}</p>
-          <p className="blog-content1">{blogPost.content1}</p>
-          <p className="blog-content2">{blogPost.content2}</p>
-          <p className="blog-content3">{blogPost.content3}</p>
-          <p className="blog-content4">{blogPost.content4}</p>
+          <p className="blog-content1">{blogPost2.content1}</p>
+          <p className="blog-content2">{blogPost2.content2}</p>
+          <p className="blog-content3">{blogPost2.content3}</p>
+          <p className="blog-content4">{blogPost2.content4}</p>
         </>
       )}
     </div>
